39578effed8290eafe75a824316ad8d5866c7320
Oleksandr Semeniuta <o.semeniuta@UAIEV8394WSL.levi9.com>
2019-09-10 16:37:13 +0300

    test

---
+++ build/precache-manifest.64ce31ab129763dba93a4cf70de0d831.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6545899c89ce5dfc3eaf92f2ab63be57",
+    "url": "/kpi-exam/build/index.html"
+  },
+  {
+    "revision": "466da76c6bd70201447a",
+    "url": "/kpi-exam/build/static/js/2.e6960d31.chunk.js"
+  },
+  {
+    "revision": "3756b35b4cc9d4a6443c",
+    "url": "/kpi-exam/build/static/js/main.52ab4d50.chunk.js"
+  },
+  {
+    "revision": "a740591d793484fe1fca",
+    "url": "/kpi-exam/build/static/js/runtime~main.79c57a4f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd07d083bf453a5d740e429ad64138ac.js"
+  "/kpi-exam/build/precache-manifest.64ce31ab129763dba93a4cf70de0d831.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kpi-exam/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
